18b85490849ba57b313a51437563fdc8e1683741
Julianne <juliannelicon@gmail.com>
2022-12-26 09:51:58 -0300

    Adicionando funcionalidade like no componente Comment

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Trash, ThumbsUp } from 'phosphor-react';
 
 import { Avatar } from './Avatar';
@@ -5,8 +7,14 @@ import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount +1)
   }
 
   return (
@@ -31,9 +39,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
