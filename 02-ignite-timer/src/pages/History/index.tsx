4ff1b8c9a422d09f37d61bad2ad76e8677a1f9a2
Julianne <juliannelicon@gmail.com>
2023-01-02 16:41:11 -0300

    Adicionando página History

---
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -0,0 +1,36 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
