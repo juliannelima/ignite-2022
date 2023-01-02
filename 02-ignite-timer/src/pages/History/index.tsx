fdda1d92656dadf274e56392f7537f01fea63fe3
Julianne <juliannelicon@gmail.com>
2023-01-02 16:57:44 -0300

    Adicionanod componente Status

--- 02-ignite-timer/src/pages/History/index.tsx
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,13 +20,25 @@ export function History() {
               <td>Conserto de débitos técnicos</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Conserto de débitos técnicos</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
           </tbody>
         </table>
