c2b3e40c087d40600df49d61e54c1253fca59051
Julianne <juliannelicon@gmail.com>
2023-01-11 10:06:54 -0300

    Adicionando formatação de data

--- 02-ignite-timer/src/pages/History/index.tsx
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -1,5 +1,9 @@
 import { useContext } from 'react'
 
+import { formatDistanceToNow } from 'date-fns'
+
+import ptBr from 'date-fns/locale/pt-BR'
+
 import { CyclesContext } from '../../contexts/CyclesContext'
 
 import { HistoryContainer, HistoryList, Status } from './styles'
@@ -26,7 +30,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBr,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
