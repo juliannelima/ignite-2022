22bd8ff30daab4cc852bb78f0c3b48786a76ab87
Julianne <juliannelicon@gmail.com>
2023-01-18 10:51:17 -0300

    Configurando Layout

--- 02-desafio-coffee-delivery/src/Router.tsx
+++ 02-desafio-coffee-delivery/src/Router.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 
+import { Layout } from './Layout'
+
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   )
 }
