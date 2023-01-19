91a0b7fbde2c4802e76f1d74948a1ad7f645ddb7
Julianne <juliannelicon@mpto.mp.br>
2023-01-19 17:43:55 -0300

    Adicionando componente Summary

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 
 import { TransactionsContainer } from './styles'
 
@@ -6,6 +7,8 @@ export function Transactions() {
   return (
     <TransactionsContainer>
       <Header />
+
+      <Summary />
     </TransactionsContainer>
   )
 }
