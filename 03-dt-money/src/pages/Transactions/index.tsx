c683a87a4e696dd4b12fec85bc2bcdc67b1e8227
Julianne <juliannelicon@mpto.mp.br>
2023-01-20 15:38:10 -0300

    Adicionando tabela de Transactions

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,14 +1,41 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
-import { TransactionsContainer } from './styles'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
-    <TransactionsContainer>
+    <div>
       <Header />
 
       <Summary />
-    </TransactionsContainer>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Lanche</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 100,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   )
 }
